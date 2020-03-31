e3e488bc185efc26f891817a617b67b75f24e37c
KevinNg <kevin.nguyen@brightstarr.com>
2020-03-31 17:02:12 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Transform1Component } from './transform1/transform1.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'transform1', component: Transform1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MalihuScrollbarModule } from 'ngx-malihu-scrollbar';
+import { CommonModule } from '@angular/common';
+import { Transform1Component } from './transform1/transform1.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Transform1Component,
   ],
   imports: [
+    HttpClientModule,
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MalihuScrollbarModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
